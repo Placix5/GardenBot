2bf52fdf0e327f7cd166451cc15db00f87698b00
Placix5 <placix5@gmail.com>
2020-07-27 18:29:05 +0200

    Servidor de prueba

---
+++ GardenBotServer/src/main/java/server/MainServer.java
@@ -0,0 +1,26 @@
+package server;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class MainServer extends AbstractVerticle {
+
+	public void start(Promise<Void> startPromise) {
+			
+		vertx.createHttpServer().requestHandler(r -> {
+			
+//			r.response().sendFile("prueba.html");
+			r.response().end("<h1>Bienvenido al servidor de pruebas</h1> Si esta viendo esto, es porque pertenece al proyecto GardenBot, en caso"
+					+ "contrario, vuelvase por donde ha venido :D");
+			
+		}).listen(8085, result -> {
+			if(result.succeeded()) {
+				startPromise.complete();
+			}else {
+				startPromise.fail(result.cause());
+			}
+		});
+		
+	}
+	
+}
