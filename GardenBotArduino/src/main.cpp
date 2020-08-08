2ce76b669a9a2a56ec263758a818fb5df41bf467
Placix5 <placix5@gmail.com>
2020-08-08 11:41:28 +0200

    Proyecto funcional

--- GardenBotArduino/src/main.cpp
+++ GardenBotArduino/src/main.cpp
@@ -1,13 +1,39 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <BlynkSimpleEsp8266.h>
 
+
+//CONEXIÓN WIFI
 WiFiClient client; //Lo usaremos para gestionar la conexión a la red wifi
 
 String SSID = "PUERTOSURFIBRA869A";
 String PASS = "A523869A";
 
+//CONEXIÓN A BLYNK
+char auth[] = "IepxU-2PjuOPqU4H9Dehh9JqG9f4Ta9L";
+char ssid[] = "PUERTOSURFIBRA869A";
+char pass[] = "A523869A";
+
 int hum;
+int manual = 0;
+int riega = 0;
+int para = 0;
+
+BLYNK_WRITE(V1)
+{
+  manual = param.asInt(); 
+}
+
+BLYNK_WRITE(V2)
+{
+  riega = param.asInt(); 
+}
+
+BLYNK_WRITE(V3)
+{
+  para = param.asInt(); 
+}
 
 void setup() {
   
@@ -23,6 +49,8 @@ void setup() {
   Serial.println("Conectado, IP: ");
   Serial.print(WiFi.localIP());
 
+  Blynk.begin(auth,ssid,pass);
+
   pinMode(D1,OUTPUT);
   pinMode(A0,INPUT);
 
@@ -33,16 +61,33 @@ void setup() {
 
 void loop() {
 
-Serial.println(analogRead(A0));
-delay(200);
+  Blynk.run();
+  hum = analogRead(A0);
 
-hum = analogRead(A0);
+  Serial.print("Humedad: ");
+  Serial.println(hum);
 
-digitalWrite(D1,LOW);
-if(hum > 500){
+  if(manual){
+    
+    if(riega){
+      digitalWrite(D1,HIGH);
+    }
+    if(para){
+      digitalWrite(D1,LOW);
+    }
 
-  digitalWrite(D1,HIGH);
+    riega = 0;
+    para = 0;
 
-}
+  }else{
+
+    digitalWrite(D1,LOW);
+    if(hum > 500){
+
+    digitalWrite(D1,HIGH);
+
+    }
+
+  }
 
 }
